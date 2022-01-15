5ee55713a5fcd09e4288f0f70a77d1018bb746ad
Pimwaree Boonnak <sxpimwaree@gmail.com>
2022-01-15 23:14:41 +0700

    edit

--- lib/classguess.dart
+++ lib/classguess.dart
@@ -1,29 +1,27 @@
 import 'dart:math';
 
 class Game {
-  static const maxRandom = 10;
+  static const maxRandom = 100;
   int? _answer;
-  var count = 0;
+  int _count = 0;
 
   Game() {
     var r = Random();
-    _answer = r.nextInt(maxRandom) + 1; //have _ in front = private
+    _answer = r.nextInt(maxRandom) + 1;
   }
+
+  int get doCount {
+    return _count++ ;
+  }
+
 
   int doGuess(int num) {
     if (num > _answer!) {
-      count++;
       return 1;
     } else if (num < _answer!) {
-      count++;
       return -1;
-    } else{
-      count++;
+    } else {
       return 0;
     }
-  }
-
-  void printCount() {
-    print("Total guess " + count.toString());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:guess_number_v2/classguess.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,6 +34,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var _controller = TextEditingController();
+
+  var game = Game();
 
   void _incrementCounter() {
     setState(() {
@@ -71,46 +75,143 @@ class _MyHomePageState extends State<MyHomePage> {
 
           child: Center(
               child: Column(
-            mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             //crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/img/guess_logo.png',
-                    height: 100.0,
-                    alignment: Alignment.center,
-                  ),
-                  Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          ' Guess ',
-                          style:
-                              TextStyle(fontSize: 45.0, color: Colors.orange),
-                        ),
-                        Text(
-                          ' The Number',
-                          style:
-                              TextStyle(fontSize: 22.0, color: Colors.orange),
-                        ),
-                      ])
-                ],
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(50),
-                child: TextField(),
-              ),
-              ElevatedButton(
-                child: Text(
-                  'Guess',
-                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/img/guess_logo.png',
+                      height: 100.0,
+                      alignment: Alignment.center,
+                    ),
+                    SizedBox(width: 9.0),
+                    Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Guess',
+                            style:
+                                TextStyle(fontSize: 45.0, color: Colors.orange),
+                          ),
+                          Text(
+                            'The Number',
+                            style:
+                                TextStyle(fontSize: 22.0, color: Colors.orange),
+                          ),
+                        ])
+                  ],
                 ),
-                onPressed: () {},
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 1.0),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.7),
+                    border: OutlineInputBorder(),
+                    hintText: 'ทายเลขตั้งแต่ 1 ถึง 100',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15.0, top: 15.0),
+                child: ElevatedButton(
+                    child: Text(
+                      'Guess',
+                      style: TextStyle(fontSize: 15.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      var input = _controller.text;
+                      var guess = int.tryParse(input);
+
+                      if (guess == null) {
+                        showDialog(
+                            context: context,
+                            barrierDismissible: false,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('RESULT'),
+                                content: Text(
+                                    "กรอกข้อมูลไม่ถูกต้อง ให้กรอกเฉพาะตัวเลขเท่านั้น"),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('OK'))
+                                ],
+                              );
+                            });
+                      }
+                      if (guess != null) {
+                        var result = game.doGuess(guess);
+                        var count = game.doCount;
+                        if (result == 0) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('RESULT'),
+                                  content: Text(
+                                      "$guess ถูกต้อง ทายไปทั้งหมด $count ครั้ง"),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('OK'))
+                                  ],
+                                );
+                              });
+                        }
+                        else if (guess != 0) {
+                         if (result == 1) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('RESULT'),
+                                  content:
+                                  Text("$guess มากไป โปรดทายใหม่อีกครั้ง"),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('OK'))
+                                  ],
+                                );
+                              });
+                        } else if (result== -1) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('RESULT'),
+                                  content:
+                                  Text("$guess น้อยไป โปรดทายใหม่อีกครั้ง"),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('OK'))
+                                  ],
+                                );
+                              });
+                        }
+                      }
+                    }
+                    }),
               ),
             ],
           )),
